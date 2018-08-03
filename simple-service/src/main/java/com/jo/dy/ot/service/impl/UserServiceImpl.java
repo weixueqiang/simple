9db2d27d1233310e288d80e617c6d6bfcd05ea09
weixueqiang <weixueqiang163@163.com>
2018-08-03 17:25:02 +0800

    fist

---
+++ simple-service/src/main/java/com/jo/dy/ot/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.jo.dy.ot.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jo.dy.ot.dao.UserMapper;
+import com.jo.dy.ot.entity.User;
+import com.jo.dy.ot.service.UserService;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+	@Resource
+	private UserMapper userMapper;
+	
+	@Override
+	@Transactional(rollbackFor=Exception.class)
+	public void save(User user) {
+		int insert = userMapper.insertSelective(user);
+		System.out.println("Userservice--"+insert);
+		int i=1/0;
+	}
+
+}
