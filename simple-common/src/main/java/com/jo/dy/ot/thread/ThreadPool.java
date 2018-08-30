1397462be8d204ea5dca96c18cbf71a47b80bfba
weixueqiang <weixueqiang163@163.com>
2018-08-30 14:05:04 +0800

    新建线程池

--- simple-common/src/main/java/com/jo/dy/ot/thread/ThreadPool.java
+++ simple-common/src/main/java/com/jo/dy/ot/thread/ThreadPool.java
@@ -1,8 +1,10 @@
 package com.jo.dy.ot.thread;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 import java.util.concurrent.TimeUnit;
@@ -15,14 +17,34 @@ public class ThreadPool {
 	private static int maximumPoolSize = 5;
 	private static long keepAliveTime = 5L;
 	/**
-	 * 无界,此时最大的线程数不起作用,已core线程数执行
+	 * 无界,此时最大的线程数maximumPoolSize不起作用,以corePoolSize线程数执行
 	 */
 	//private static BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>();
-	private static BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>(2);
 	/**
-	 * 该策略当没有线程使用时,会使用调用 该线程池的线程  来执行任务--很有可能造成当前线程也被阻塞
+	 * 有界,初始化需要指定长度,当队列已满,则会创建新线程,最大数量到maximumPoolSize
+	 */
+	private static BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(3);
+	/**
+	 * 直接提交策略表示线程池不对任务进行缓存,新进任务直接提交给线程池，当线程池中没有空闲线程时，创建一个新的线程处理此任务,这种策略需要线程池具有无限增长的可能性
+	 */
+	//private static BlockingQueue<Runnable> workQueue = new SynchronousQueue<Runnable>();
+	/**
+	 * 该策略当没有线程使用时,会使用调用执行 该线程池的线程  来执行任务--很有可能造成调用线程池的线程被阻塞
 	 */
 	private static RejectedExecutionHandler handler  = new ThreadPoolExecutor.CallerRunsPolicy();//new CallerRunsPolicy();
+	
+	/**
+	 * jdk默认策略，队列满并线程满时直接拒绝添加新任务，并抛出异常
+	 */
+	//private static RejectedExecutionHandler handler=new ThreadPoolExecutor.AbortPolicy();
+	/**
+	 * 队列满并线程满时丢弃新任务，不抛出异常,不能执行的任务将被删除 
+	 */
+	//private static RejectedExecutionHandler handler=new ThreadPoolExecutor.DiscardPolicy();
+	/**
+	 * 删除队列头部任务
+	 */
+	//private static RejectedExecutionHandler handler=new ThreadPoolExecutor.DiscardOldestPolicy();
 	
 	private static ThreadPoolExecutor threads = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, workQueue,
 			handler);
--- simple-common/src/main/java/com/jo/dy/ot/thread/ThreadTest.java
+++ simple-common/src/main/java/com/jo/dy/ot/thread/ThreadTest.java
@@ -1,13 +1,20 @@
 package com.jo.dy.ot.thread;
 
+import java.util.Scanner;
+
 public class ThreadTest {
 
 	public static void main(String[] args) {
+		System.out.println(canFinishMethodTest());
+		System.out.println("main方法结束---");
+	}
+	
+	public static String canFinishMethodTest() {
 		ThreadPool instance = ThreadPool.getInstance();
 		for(int i=0;i<10;i++) {
 			instance.excute(new TestRun(i));
 		}
-		instance.close();
+		return "ok";
 	}
 	
 }
@@ -24,8 +31,14 @@ class TestRun implements Runnable{
 	public void run() {
 		try {
 			System.out.println(index);
-			Thread.sleep(3000);
-			System.out.println(Thread.currentThread().getName()+" :"+this.index);
+			String name = Thread.currentThread().getName();
+//			if(name.equals("main")) {
+//				Scanner scan = new Scanner(System.in);
+//				System.out.println("策略阻塞:"+index);
+//				scan.nextLine();
+//			}
+			Thread.sleep(2000);
+			System.out.println(name+" :"+this.index);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			   e.printStackTrace();
